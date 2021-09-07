ecb31000f798278adb0aff4857bdd15602793fb2
Tonystrak69 <89418931+Tonystrak69@users.noreply.github.com>
2021-09-07 09:56:51 +0530

    Update player.py

--- plugins/bot/player.py
+++ plugins/bot/player.py
@@ -570,11 +570,11 @@ allcmd = ["play", "current", "playlist", "song", f"song@{USERNAME}", f"play@{USE
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/tonyleechandmirror"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/tony_leech_zone"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("LEECH+MIRROR", url="https://t.me/tony_leech_zone"),
             ]
          ]
     k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
