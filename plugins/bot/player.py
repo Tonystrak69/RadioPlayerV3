cf0df8fac243f991bcf6583118feffe269f002cd
Tonystrak69 <89418931+Tonystrak69@users.noreply.github.com>
2021-09-07 10:00:34 +0530

    Update player.py

--- plugins/bot/player.py
+++ plugins/bot/player.py
@@ -577,5 +577,5 @@ async def not_chat(_, m: Message):
                 InlineKeyboardButton("LEECH+MIRROR", url="https://t.me/tony_leech_zone"),
             ]
          ]
-    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
+    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 😈", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
